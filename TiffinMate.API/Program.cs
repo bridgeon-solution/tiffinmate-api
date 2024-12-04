c95acc4dcf12dd880f710c91e91a12c9055a35eb
Sabith Mk <sabithmk323@gmail.com>
2024-12-04 11:47:47 +0530

    Update Program.cs

--- TiffinMate.API/Program.cs
+++ TiffinMate.API/Program.cs
@@ -23,11 +23,9 @@ namespace TiffinMate.API
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+           
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 

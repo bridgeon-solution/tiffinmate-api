ec7007fa5d024a2d2f99dab3e724f4e4d9227068
Suhaila <suhailathalappill@gmail.com>
2025-01-13 15:27:09 +0530

    feature: user order history

--- TiffinMate.BLL/Services/OrderService/OrderService.cs
+++ TiffinMate.BLL/Services/OrderService/OrderService.cs
@@ -323,13 +323,17 @@ namespace TiffinMate.BLL.Services.OrderService
 
 
         //AllOrders
-        public async Task<AllOrderDTO> GetUserOrders(int page, int pageSize, string search = null, string filter = null)
+        public async Task<AllOrderDTO> GetUserOrders(int page, int pageSize, string search = null, string filter = null,Guid? userId=null)
         {
-            
-            var orders = await _context.order
-                 .Include(o=>o.provider).Include(o=>o.user).Include(o => o.details).ThenInclude(d=>d.Category)
-                .Where(o => o.payment_status) 
-                .ToListAsync();
+
+            var orders = _context.order
+                 .Include(o => o.provider).Include(o => o.user).Include(o => o.details).ThenInclude(d => d.Category)
+                .Where(o => o.payment_status);
+            if(userId!= null)
+            {
+                orders = orders.Where(o => o.user_id == userId);
+            }
+       
 
             var result = orders.Select(order => new OrderDetailsResponseDTO
             {

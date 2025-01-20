4a5a5825c7b6808fa34020f5e53b7af9e0681732
Suhaila <suhailathalappill@gmail.com>
2025-01-20 21:50:45 +0530

    feat: update migration file

---
+++ TiffinMate.DAL/Migrations/20250120161932_migration_on_20240120.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TiffinMate.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class migration_on_20240120 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_subscriptions_menus_Menuid",
+                table: "subscriptions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_subscriptions_Menuid",
+                table: "subscriptions");
+
+            migrationBuilder.DropColumn(
+                name: "Menuid",
+                table: "subscriptions");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_subscriptions_menu_id",
+                table: "subscriptions",
+                column: "menu_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_subscriptions_menus_menu_id",
+                table: "subscriptions",
+                column: "menu_id",
+                principalTable: "menus",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_subscriptions_menus_menu_id",
+                table: "subscriptions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_subscriptions_menu_id",
+                table: "subscriptions");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Menuid",
+                table: "subscriptions",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_subscriptions_Menuid",
+                table: "subscriptions",
+                column: "Menuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_subscriptions_menus_Menuid",
+                table: "subscriptions",
+                column: "Menuid",
+                principalTable: "menus",
+                principalColumn: "id");
+        }
+    }
+}
